c01152ed1b437b37c84bbae891b57291affa49c6
sudheer <sudheer.mulinti@gmail.com>
2021-01-02 21:53:40 +0530

    first commit - adding intial java spring boot app

---
+++ src/main/java/site/mvnr/DemoJS/DemoJsApplication.java
@@ -0,0 +1,13 @@
+package site.mvnr.DemoJS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/mvnr/DemoJS/DemoJsApplicationTests.java
@@ -0,0 +1,13 @@
+package site.mvnr.DemoJS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
