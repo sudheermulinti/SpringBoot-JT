8fadab7be6f4544f23970c0f2933645a1005077e
sudheer <sudheer.mulinti@gmail.com>
2021-01-02 22:18:28 +0530

    added rest path for /

--- src/main/java/site/mvnr/DemoJS/DemoJsApplication.java
+++ src/main/java/site/mvnr/DemoJS/DemoJsApplication.java
@@ -4,7 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
 public class DemoJsApplication {
+
+    @GetMapping("/")
+	public String home (){
+
+		return "Welcome To JS...!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoJsApplication.class, args);
